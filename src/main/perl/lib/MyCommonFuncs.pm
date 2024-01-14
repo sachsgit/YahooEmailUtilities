adff40abcb2b1c2a9aae83bcbdeec059c3650d8c
sachsgit <25045377+sachsgit@users.noreply.github.com>
2024-01-14 17:07:08 -0500

    Clean-Up of MyCommonFuncs.pm

--- src/main/perl/lib/MyCommonFuncs.pm
+++ src/main/perl/lib/MyCommonFuncs.pm
@@ -1,6 +1,7 @@
 package myCommonFuncs;
 use strict;
 
+use Cwd;
 use Data::Dump qw(dump);
 use Encode;
 use File::Copy qw(copy);
@@ -32,7 +33,7 @@ use HTTP::Cookies;
 use constant DEBUG         => FALSE; # TRUE
 use constant DEBUG_CLEANUP => FALSE; # TRUE
 use constant DEBUG_DECODE  => FALSE; # TRUE
-use constant DEBUG_DUMP    => TRUE;
+use constant DEBUG_DUMP    => FALSE; # TRUE
 use constant DEBUG_HEAD    => FALSE; # TRUE
 use constant DEBUG_ISCOM   => FALSE; # TRUE
 
@@ -82,12 +83,6 @@ sub geturl {
 
     my $ua = LWP::UserAgent->new;
     $ua->agent( USER_AGENT_STRING );
-    $ua->cookie_jar(
-        HTTP::Cookies->new(
-                file => 'mycookies.txt',
-                autosave => 1
-        )
-    );
     my $req = GET $url;
     my $res = $ua->request($req);
 
@@ -101,148 +96,6 @@ sub geturl {
         print "Status Line: " . $res->status_line . "\n";
     }
 }
-
-sub geturl_old {
-     my $myUrl = shift;
-     die "No URL to retrieve" unless defined $myUrl;
-
-     my $ua = LWP::UserAgent->new;
-
-     # Firefox 55.0
-     my $myAgent = "Mozilla/5.0 (Android 4.4; Tablet; rv:41.0)"
-          . " Gecko/41.0 Firefox/55.0";
-
-     $ua->agent($myAgent);
-     $ua->timeout(30);
-     #$ua->env_proxy;
-
-     my $headerResponse = $ua->head($myUrl);
-     print STDERR __LINE__ . ": Status: " . $headerResponse->status_line
-          . "\n" if (DEBUG_HEAD == TRUE);
-
-     my $response = $ua->get($myUrl);
-
-     warn "Couldn't get contents of \"$myUrl\"\n" unless defined $response;
-
-     my $contents = "";
-     if ($response->is_success && $response->content ne "") {
-          $contents = $response->content;
-          $contents = decode('utf-8', $contents);
-          dumpContents($contents) if (DEBUG_DUMP == TRUE);
-          return ($contents);
-     } elsif (!$response->is_success) {
-          die "GetUrl returns \"" . $response->status_line() . "\"\n"
-               . "URL: \"" . $myUrl . "\"\n";
-     } else {
-          print STDERR __LINE__ . ": Using lynx instead of "
-               . "LWP::UserAgent\n" if DEBUG == TRUE;
-          my $lynxcmd = capturex("which", "lynx");
-          $lynxcmd = trim($lynxcmd);
-          die "Cannot find lynx on system\n" if $lynxcmd eq "";
-          my @arParams;
-          $arParams[0] = "-dump";
-          $arParams[1] = "-source";
-          $arParams[2] = $myUrl;
-          $contents = capturex($lynxcmd, @arParams);
-          $contents = decode('utf-8', $contents);
-          dumpContents($contents) if (DEBUG_DUMP == TRUE);
-          return $contents;
-     } # End If-ElsIf-Else
-
-     if ($contents =~ /class='errortext'>Age Consent Required/) {
-         $response = $ua->get($myUrl . "&ageconsent=ok&warning=4");
-         $contents = $response->content;
-         $contents = decode('utf-8', $contents);
-     } elsif ($contents =~ /This work could have adult content. If you proceed you have agreed that you are willing to see such content./) {
-         $response = $ua->get($myUrl . "?view_adult=true");
-         $contents = $response->content;
-         $contents = decode('utf-8', $contents);
-         if (DEBUG_DUMP == TRUE) {
-             print "Dumping Contents ...";
-             dumpContents($contents);
-             print " ... dumped.\n";
-         }
-     }
-     print STDERR __LINE__ . ": Size of content is " . length($contents) . "\n"
-          if (DEBUG_DECODE == TRUE);
-     return ($contents);
-} # End Sub geturl_old
-
-sub isComplete {
-     my ($lines, $chpt) = @_;
-
-     print "Checking for completeness\n" if (DEBUG_ISCOM == TRUE);
-
-     if ($lines =~ /http(s)?:\/\/(www\.)?tthfanfic/i) {
-          print STDERR __LINE__ . " TTH found\n" if (DEBUG_ISCOM == TRUE);
-          my $te = HTML::TableExtract->new( headers => [
-               qw(Chapters Complete)] );
-          (my $utf8lines = $lines) =~ s/([^[\x20-\x7F])/"&#" . ord($1) . ";"/eg;
-          print STDERR __LINE__ . " Size: " . length($lines) . " Size: " .
-               length($utf8lines) . "\n" if (DEBUG_ISCOM == TRUE);
-          $te->parse($utf8lines);
-          my $ts = $te->first_table_found;
-          my $numChpts = trim($ts->cell(0,0));
-          my $isComplete = trim($ts->cell(0,1));
-          if (DEBUG_ISCOM == TRUE) {
-               print STDERR __LINE__ . " \$numChpts is \"$numChpts\"\n";
-               print STDERR __LINE__ . " \$chpt is \"$chpt\"\n";
-               print STDERR __LINE__ . " \$isComplete is \"" .
-                    displayTF($isComplete) . "\"\n";
-          } # End If DEBUG_ISCOM is TRUE
-          return TRUE if ($isComplete eq 'Yes');
-     } elsif ($lines =~ /http(s)?:\/\/(www\.)?fanfiction\.net/i
-               && $lines !~ /adult-fanfiction/i) {
-          print STDERR __LINE__ . ": FFN found\n" if (DEBUG_ISCOM == TRUE);
-          my @lines = split /\n/, $lines;
-          my @NotFound = grep /<hr size=1 noshade>Chapter not found./, @lines;
-          print STDERR __LINE__ . ": \$#NotFound: $#NotFound\n" if (DEBUG_ISCOM == TRUE);
-          return FALSE if $#NotFound == 0;
-
-          @NotFound = grep /Story not found/, @lines;
-          print STDERR __LINE__ . ": \$#NotFound: $#NotFound\n" if (DEBUG_ISCOM == TRUE);
-          return FALSE if $#NotFound == 0;
-
-          my $lastChpt = 1;
-          my $isComplete = FALSE;
-          print STDERR __LINE__ . ": Length of \$lines " . length($lines) . "\n"
-               if (DEBUG_ISCOM == TRUE);
-          my $tree = new HTML::TreeBuilder::XPath->new;
-          $tree->parse($lines);
-          my @Status = $tree->findnodes('//span[@class="xgray'
-              . ' xcontrast_txt"]');
-          print STDERR __LINE__ . ": Status Count: $#Status\n" if (DEBUG_ISCOM == TRUE);
-          my $myStatus = ($Status[0]->as_HTML || $Status[0]);
-          if ($myStatus =~ /(Status: Complete)/) {
-               print STDERR __LINE__ . " $1\n" if (DEBUG_ISCOM == TRUE);
-               $isComplete = TRUE;
-          }
-          $lastChpt = $1 if ($lines =~ /Chapters: (\d+)/);
-          if (DEBUG_ISCOM == TRUE) {
-               print STDERR __LINE__ . " \$lastChpt is \"$lastChpt\"\n";
-               print STDERR __LINE__ . " \$chpt is \"$chpt\"\n";
-               print STDERR __LINE__ . " \$isComplete is \"" .
-                    displayTF($isComplete) . "\"\n";
-          }
-          return TRUE if ($isComplete == TRUE);
-     } elsif ($lines =~ /ficwad/i) {
-         print STDERR __LINE__ . " FW Found\n" if (DEBUG_ISCOM == TRUE);
-         my $lastChpt = 1;
-         my $tree = new HTML::TreeBuilder::XPath->new;
-         $tree->parse($lines);
-         my @chpts = $tree->findnodes('//div[@class="chapterform"]'
-              . '/form/select/option');
-         $lastChpt = (($#chpts + 1)/2) - 1 if ($lines =~
-              /<div class="chapterform"/);
-         print STDERR __LINE__ . " \$lastChpt is \"$lastChpt\"\n"
-              . "\$chpt is \"$chpt\"\n"
-              if (DEBUG_ISCOM == TRUE);
-         return TRUE if ($lines =~
-             /\d+&nbsp;words\s+-\s+Complete<\/p>/);
-     } # End if
-
-     return FALSE
-} #-- end isComplete
 
 sub cleanup {
      my $name = shift;
@@ -353,6 +206,17 @@ sub narrow_char {
       if (length($_[0]) * 3 == do { use bytes; length $_[0] } );
     $_[0]; 
 
+}
+
+sub getSecret {
+     my $path = $ENV{'HOME'} || getcwd();
+     my $sourceFile = "${path}/../resources/.ak";
+     open(IN, $sourceFile) || die "Cannot open $sourceFile\n$!\n";
+     my @lines = <IN>;
+     close(IN);
+     my $value = $lines[0];
+     chomp($value);
+     return $value;
 }
 
 ### -- End of Subroutines --- ###
